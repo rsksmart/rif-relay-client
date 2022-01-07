39845f2260dbc6dcac061b84cbe5c5e761ec0da5
mortelli <ortellimarcelo@gmail.com>
2022-01-07 11:08:07 -0300

    revenue sharing POC (#9)
    * Estimate the transaction cost if the tokenAmount is missing or zero
    
    * Run prettier
    
    * Update dist
    
    * Do not estimate transasction if the tokenAmount is 0
    
    * Update dist
    
    * Update src/Conversions.ts
    
    Co-authored-by: mortelli <ortellimarcelo@gmail.com>
    
    * PR Fix
    
    Co-authored-by: Antonio Morrone <antonio@iovlabs.org>
    Co-authored-by: Antonio Morrone <dev@antomor.com>

--- src/RelayClient.ts
+++ src/RelayClient.ts
@@ -41,6 +41,7 @@ import {
     ValidateRequestEvent
 } from './RelayEvents';
 import { toBN, toHex } from 'web3-utils';
+import { getGasCostInWei, getTRifWei } from './Conversions';
 
 export const GasPricePingFilter: PingFilter = (
     pingResponse,
@@ -200,7 +201,7 @@ export class RelayClient {
     /**
      * Can be used to get an estimate of the maximum possible gas to be used by the transaction by using
      * a linear fit.
-     * It has the advangate of not requiring the user to sign the transaction in the relay calls
+     * It has the advantage of not requiring the user to sign the transaction in the relay calls
      * If the transaction details are for a deploy, it won't use a linear fit
      * @param transactionDetails
      * @param relayWorker
@@ -631,6 +632,39 @@ export class RelayClient {
                 relayInfo
             )} transaction: ${JSON.stringify(transactionDetails)}`
         );
+        if ([undefined, null].includes(transactionDetails.tokenAmount)) {
+            log.debug('Calculating maxPossibleGas...');
+
+            const txDetailsClone: EnvelopingTransactionDetails = {
+                ...transactionDetails,
+                // a dump value, just to allow the correct estimation, including the token transfer
+                tokenAmount: web3.utils.toWei('1').toString()
+            };
+            const maxPossibleGas = await this.estimateMaxPossibleRelayGas(
+                txDetailsClone,
+                relayInfo.pingResponse.relayWorkerAddress
+            );
+            log.debug('maxPossibleGas', maxPossibleGas);
+            const tRifValueWei = getTRifWei(
+                getGasCostInWei(
+                    toBN(maxPossibleGas),
+                    toBN(transactionDetails.gasPrice)
+                )
+            );
+            log.info('RequestFees: max possible cost in tRIFWei', tRifValueWei);
+            // FIXME: We should add some additional charge here, instead of using just the tx estimation
+            transactionDetails = {
+                ...transactionDetails,
+                tokenAmount: tRifValueWei
+            };
+            transactionDetails.tokenGas = (
+                await this.estimateTokenTransferGas(
+                    transactionDetails,
+                    relayInfo.pingResponse.relayWorkerAddress
+                )
+            ).toString();
+        }
+
         let httpRequest: RelayTransactionRequest | DeployTransactionRequest;
         let acceptCallResult;
 
@@ -669,6 +703,9 @@ export class RelayClient {
                 };
             }
         }
+
+        // We don't need to check the user balance with a call to 'erc20.balanceOf'
+        // because the same check is performed when RelayVerifier is called
 
         if (acceptCallResult.reverted) {
             const message = 'local view call reverted';
@@ -860,7 +897,8 @@ export class RelayClient {
                 tokenContract:
                     transactionDetails.tokenContract ?? constants.ZERO_ADDRESS,
                 collectorContract:
-                        transactionDetails.collectorContract ?? constants.ZERO_ADDRESS,
+                    transactionDetails.collectorContract ??
+                    constants.ZERO_ADDRESS
             },
             relayData: {
                 gasPrice,
