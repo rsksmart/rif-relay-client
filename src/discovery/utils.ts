b8dad64f4e4bad2c092d4ab81d8f2ad5d23be6ad
Juraj <juraj.piar@icloud.com>
2022-11-29 09:57:02 +0000

    PP-501: Adapts SmartWalletDiscovery module (#46)
    * feat(ethers): adapts SmartWalletDiscovery module
    
    * feat(ethers): adapts discovery by ext pub keys

---
+++ src/discovery/utils.ts
@@ -0,0 +1,311 @@
+import { constants, getDefaultProvider, providers } from 'ethers';
+import {
+  getAddress,
+  HDNode,
+  keccak256,
+  solidityKeccak256,
+} from 'ethers/lib/utils';
+
+import { langCz as cz } from '@ethersproject/wordlists/lib/lang-cz';
+import { langEn as en } from '@ethersproject/wordlists/lib/lang-en';
+import { langEs as es } from '@ethersproject/wordlists/lib/lang-es';
+import { langFr as fr } from '@ethersproject/wordlists/lib/lang-fr';
+import { langIt as it } from '@ethersproject/wordlists/lib/lang-it';
+import { langJa as ja } from '@ethersproject/wordlists/lib/lang-ja';
+import { langKo as ko } from '@ethersproject/wordlists/lib/lang-ko';
+import {
+  langZhCn as zh_cn,
+  langZhTw as zh_tw,
+} from '@ethersproject/wordlists/lib/lang-zh';
+import type { SmartWalletFactory } from '@rsksmart/rif-relay-contracts/dist/typechain-types/contracts/factory/SmartWalletFactory';
+import { ERC20__factory } from '@rsksmart/rif-relay-contracts/dist/typechain-types/factories/@openzeppelin/contracts/token/ERC20/ERC20__factory';
+import { SmartWalletFactory__factory } from '@rsksmart/rif-relay-contracts/dist/typechain-types/factories/contracts/factory';
+const SHA3_NULL_S = keccak256('0x');
+
+type Account = {
+  eoaAccount: string;
+  swAccounts: string[];
+};
+
+// TODO: copied from @ethersproject/wordlists/src.ts/wordlists.ts as that doesn't allow for locale type extraction. Remove (including imports) when this is fixed (https://github.com/ethers-io/ethers.js/pull/3514).
+const wordlists = {
+  cz,
+  en,
+  es,
+  fr,
+  it,
+  ja,
+  ko,
+  zh: zh_cn,
+  zh_cn,
+  zh_tw,
+} as const;
+
+type Locale = keyof typeof wordlists; //TODO: hopefully this will be added to ethers.js (https://github.com/ethers-io/ethers.js/pull/3514)
+
+// TODO: check that the optional params have to be optional
+type Config = {
+  eoaGap: number;
+  sWalletGap: number;
+  searchBalance: boolean;
+  searchNonce: boolean;
+  searchTokenBalance: boolean;
+  searchDeployEvents: boolean;
+  network: string; //TODO: potentially restictable by program-wide config
+  factory: string;
+  isCustomWallet: boolean;
+  mnemonicLanguage: Locale;
+  recoverer: string;
+  logic: string;
+  logicParamsHash: string;
+  reconnectionAttempts: number;
+  tokens: string[];
+};
+
+const DEFAULT_DISCOVERY_CONFIG: Partial<Config> = {
+  eoaGap: 20,
+  sWalletGap: 20,
+  searchBalance: true,
+  searchNonce: true,
+  searchTokenBalance: true,
+  searchDeployEvents: true,
+  network: 'mainnet', //FIXME: this should probably be something else. Also should be linked/mapped to the network id or address defined in program-wide config or env vars;
+  mnemonicLanguage: 'en',
+  recoverer: constants.AddressZero,
+  tokens: [],
+};
+
+//Account discovery https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki#account-discovery
+const DEFAULT_DERIVATION_PATH_PURPOSE = 44;
+const RSK_COIN_TYPE = 137; // src: https://github.com/satoshilabs/slips/blob/master/slip-0044.md
+const RSK_TESTNET_COIN_TYPE = 37310; // src: https://github.com/rsksmart/RSKIPs/blob/master/IPs/RSKIP57.md
+const DEFAULT_DERIVATION_PATH_CHANGE = 0;
+
+type Setup = {
+  config: Config;
+  rootNode?: HDNode;
+  bytecodeHash: string;
+  smartWalletFactory: SmartWalletFactory;
+  provider: providers.BaseProvider;
+};
+
+const setupDiscovery = async (
+  configOverride: Config,
+  mnemonic?: string,
+  password?: string
+): Promise<Setup> => {
+  const config = {
+    ...DEFAULT_DISCOVERY_CONFIG,
+    ...configOverride,
+  };
+
+  const rootNode = mnemonic
+    ? HDNode.fromMnemonic(
+        mnemonic,
+        password,
+        wordlists[config.mnemonicLanguage]
+      )
+    : undefined;
+  const provider = getDefaultProvider(config.network);
+
+  const smartWalletFactory = SmartWalletFactory__factory.connect(
+    config.factory,
+    provider
+  );
+
+  const creationByteCode = await smartWalletFactory.getCreationBytecode();
+  const bytecodeHash = keccak256(creationByteCode);
+  const logic = config.logic ?? constants.AddressZero;
+  const logicParamsHash = config.logicParamsHash ?? SHA3_NULL_S;
+
+  return {
+    config: {
+      ...config,
+      logic,
+      logicParamsHash,
+    },
+    rootNode,
+    bytecodeHash,
+    smartWalletFactory,
+    provider,
+  };
+};
+
+const constructDerivationPath = (
+  addressIdx: number,
+  hardenedIdx: number,
+  coinType?: number,
+  isTestNet = false,
+  purpose = DEFAULT_DERIVATION_PATH_PURPOSE,
+  change: 0 | 1 = DEFAULT_DERIVATION_PATH_CHANGE
+): `m/${string}` => {
+  return `m/${purpose}'/${
+    coinType || isTestNet ? RSK_TESTNET_COIN_TYPE : RSK_COIN_TYPE
+  }'/${hardenedIdx}'/${change}/${addressIdx}`;
+};
+
+const getSWAddress = (
+  config: Config,
+  ownerEOA: string,
+  walletIndex: number,
+  bytecodeHash: string
+): string => {
+  const isCustom = config.logic && config.logicParamsHash;
+  const salt = isCustom
+    ? solidityKeccak256(
+        ['address', 'address', 'address', 'bytes32', 'uint256'],
+        [
+          ownerEOA,
+          config.recoverer,
+          config.logic,
+          config.logicParamsHash ?? SHA3_NULL_S,
+          walletIndex,
+        ]
+      )
+    : solidityKeccak256(
+        ['address', 'address', 'uint256'],
+        [ownerEOA, config.recoverer, walletIndex]
+      );
+
+  const data: string = solidityKeccak256(
+    ['bytes1', 'address', 'bytes32', 'bytes32'],
+    ['0xff', config.factory, salt, bytecodeHash]
+  );
+
+  const address = `0x${data.slice(26, data.length)}`;
+
+  return getAddress(address);
+};
+
+const checkHasActivity = async (
+  accountAddr: string,
+  config: Config,
+  provider: providers.Provider
+): Promise<boolean> => {
+  const hasBalance = (await provider.getBalance(accountAddr, 'latest')).gt(0);
+
+  if (hasBalance) {
+    return true;
+  }
+
+  const hasTx = (await provider.getTransactionCount(accountAddr, 'latest')) > 0;
+
+  if (hasTx) {
+    return true;
+  }
+
+  const erc20Balances = await Promise.all(
+    config.tokens.map((token) =>
+      ERC20__factory.connect(token, provider).balanceOf(accountAddr)
+    )
+  );
+
+  const hasERC20Balance = Boolean(
+    erc20Balances.find((balance) => balance.gt(0))
+  );
+
+  if (hasERC20Balance) {
+    return true;
+  }
+
+  return false;
+};
+
+const findHardenedNodeActivity = async (
+  { config, rootNode, provider, bytecodeHash }: Required<Setup>,
+  hardenedAccIdx: number
+): Promise<Account[]> => {
+  let accounts: Account[] = [];
+  let inactivityCountdown = config.eoaGap;
+  let eoaIndex = 0;
+
+  while (inactivityCountdown > 0) {
+    const { address: eoaAccount } = rootNode.derivePath(
+      constructDerivationPath(eoaIndex, hardenedAccIdx)
+    );
+    const eoaActivityFound = await checkHasActivity(
+      eoaAccount,
+      config,
+      provider
+    );
+
+    const swAccounts = await findSWActivity(
+      eoaAccount,
+      config,
+      bytecodeHash,
+      provider
+    );
+
+    if (swAccounts.length > 0 || eoaActivityFound) {
+      inactivityCountdown = config.eoaGap;
+
+      accounts = [
+        ...accounts,
+        {
+          eoaAccount,
+          swAccounts,
+        },
+      ];
+    } else {
+      inactivityCountdown -= 1;
+    }
+    eoaIndex++;
+  }
+
+  return accounts;
+};
+
+const findSWActivity = async (
+  eoaAddr: string,
+  config: Config,
+  bytecodeHash: string,
+  provider: providers.Provider
+): Promise<string[]> => {
+  let inactivityCountdown = config.sWalletGap;
+  let swIndex = 0;
+  let swAccounts: string[] = [];
+
+  while (inactivityCountdown > 0) {
+    // originally <=gapLimit, but rskj rpc crashes
+    const currentSWAddress = getSWAddress(
+      config,
+      eoaAddr,
+      swIndex,
+      bytecodeHash
+    );
+
+    const hasActivity = await checkHasActivity(
+      currentSWAddress,
+      config,
+      provider
+    );
+
+    if (hasActivity) {
+      swAccounts = [...swAccounts, currentSWAddress];
+      inactivityCountdown = config.sWalletGap;
+    } else {
+      inactivityCountdown -= 1;
+    }
+
+    swIndex++;
+  }
+
+  return swAccounts;
+};
+
+export type { Config, Account, Setup };
+export {
+  getSWAddress,
+  findSWActivity,
+  findHardenedNodeActivity,
+  constructDerivationPath,
+  setupDiscovery,
+  checkHasActivity,
+};
+export {
+  DEFAULT_DISCOVERY_CONFIG,
+  DEFAULT_DERIVATION_PATH_PURPOSE,
+  RSK_COIN_TYPE,
+  RSK_TESTNET_COIN_TYPE,
+  DEFAULT_DERIVATION_PATH_CHANGE,
+};
