abe1c3839f36499718eb037319cee68733c65b95
AndresQuijano <66392159+AndresQuijano@users.noreply.github.com>
2022-12-22 11:44:46 -0500

    PP601: Port the GasEstimator from the server to the client (#62)
    * feat(GasEstimator): gas estimator for relay and deploy transactions
    
    refactor(GasEstimator): renaming
    
    * refactor(gasEstimator): modify tokenAmount
    
    * chore(fixme): fix the ts build error
    
    Co-authored-by: Francisco Tobar <francisco.tobar@iovlabs.org>
    Co-authored-by: Antonio Morrone <antonio@iovlabs.org>

---
+++ src/gasEstimator/gasEstimator.ts
@@ -0,0 +1,50 @@
+import { BigNumber, utils } from 'ethers';
+import { isDeployRequest } from '../common/relayRequest.utils';
+import type { EnvelopingTxRequest } from '../common/relayTransaction.types';
+import RelayClient from '../RelayClient';
+import {
+  standardMaxPossibleGasEstimation,
+  linearFitMaxPossibleGasEstimation,
+} from './utils';
+
+const estimateRelayMaxPossibleGas = async (
+  request: EnvelopingTxRequest,
+  relayWorkerAddress: string
+): Promise<BigNumber> => {
+  const {
+    relayRequest,
+    metadata: { signature },
+  } = request;
+
+  const { tokenContract } = relayRequest.request;
+  const { feesReceiver, callForwarder, gasPrice } = relayRequest.relayData;
+
+  const relayClient = new RelayClient();
+
+  const isSmartWalletDeploy = isDeployRequest(relayRequest);
+
+  //FIXME validate how to generate the smart wallet address
+  const preDeploySWAddress = isSmartWalletDeploy ? undefined : undefined;
+
+  const tokenEstimation = await relayClient.estimateTokenTransferGas({
+    tokenContract,
+    tokenAmount: utils.formatUnits(1, 'wei'), // tokenAmount should be a value different from zero to simulate the transfer estimation
+    feesReceiver,
+    isSmartWalletDeploy,
+    preDeploySWAddress,
+    callForwarder,
+    gasPrice,
+  });
+
+  if (signature > '0x0') {
+    return await standardMaxPossibleGasEstimation(
+      request,
+      relayWorkerAddress,
+      tokenEstimation
+    );
+  }
+
+  return await linearFitMaxPossibleGasEstimation(relayRequest, tokenEstimation);
+};
+
+export { estimateRelayMaxPossibleGas };
