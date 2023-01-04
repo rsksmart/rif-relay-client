6f5f82346c9ebfd34eeb57189684de504d4f6d81
Christos <otarola.christos@gmail.com>
2023-01-04 10:44:55 -0500

    chore: linted files

--- src/handlers/RelayProvider.ts
+++ src/handlers/RelayProvider.ts
@@ -2,53 +2,58 @@ import type { LogDescription } from 'ethers/lib/utils';
 import log from 'loglevel';
 import type { RelayStatus } from '../RelayProvider';
 
-export type RelayTransactionEvents = 'TransactionRelayedButRevertedByRecipient'
+export type RelayTransactionEvents =
+  | 'TransactionRelayedButRevertedByRecipient'
   | 'TransactionRelayed'
-  | 'Deployed'
+  | 'Deployed';
 
 export interface RelayTransactionHandler<E> {
-    process: (event: E, logDescription: LogDescription) => RelayStatus
-    default: () => RelayStatus
+  process: (event: E, logDescription: LogDescription) => RelayStatus;
+  default: () => RelayStatus;
 }
 
 export const handlers = {
-    TransactionRelayedButRevertedByRecipient: (recipientRejectedEvents: LogDescription) => {
-        const { reason } = recipientRejectedEvents.args;
-            const revertReason = (reason as string);
+  TransactionRelayedButRevertedByRecipient: (
+    recipientRejectedEvents: LogDescription
+  ) => {
+    const { reason } = recipientRejectedEvents.args;
+    const revertReason = reason as string;
 
-            return {
-                relayRevertedOnRecipient: true,
-                transactionRelayed: false,
-                reason: revertReason
-            };
-    },
-    TransactionRelayed: () => {
-        return {
-            relayRevertedOnRecipient: false,
-            transactionRelayed: true
-        };
-    },
-    Deployed: () => {
-        return {
-            relayRevertedOnRecipient: false,
-            transactionRelayed: true
-        };
-    }
-}
+    return {
+      relayRevertedOnRecipient: true,
+      transactionRelayed: false,
+      reason: revertReason,
+    };
+  },
+  TransactionRelayed: () => {
+    return {
+      relayRevertedOnRecipient: false,
+      transactionRelayed: true,
+    };
+  },
+  Deployed: () => {
+    return {
+      relayRevertedOnRecipient: false,
+      transactionRelayed: true,
+    };
+  },
+};
 
-export const relayTransactionHandler: RelayTransactionHandler<RelayTransactionEvents> = {
-    process (event: RelayTransactionEvents, logDescription: LogDescription) {  
+export const relayTransactionHandler: RelayTransactionHandler<RelayTransactionEvents> =
+  {
+    process(event: RelayTransactionEvents, logDescription: LogDescription) {
       return handlers[event](logDescription);
     },
     default() {
-        log.info(
-            'Neither TransactionRelayed, Deployed, nor TransactionRelayedButRevertedByRecipient events found. This might be a non-enveloping transaction.'
-        );
+      log.info(
+        'Neither TransactionRelayed, Deployed, nor TransactionRelayedButRevertedByRecipient events found. This might be a non-enveloping transaction.'
+      );
 
-        return {
-            relayRevertedOnRecipient: false,
-            transactionRelayed: false,
-            reason: 'Neither TransactionRelayed, Deployed, nor TransactionRelayedButRevertedByRecipient events found. This might be a non-enveloping transaction.'
-        }; 
-    }
-  }
+      return {
+        relayRevertedOnRecipient: false,
+        transactionRelayed: false,
+        reason:
+          'Neither TransactionRelayed, Deployed, nor TransactionRelayedButRevertedByRecipient events found. This might be a non-enveloping transaction.',
+      };
+    },
+  };
--- test/handlers/RelayProvider.test.ts
+++ test/handlers/RelayProvider.test.ts
@@ -1,67 +1,73 @@
-import type { LogDescription } from "@ethersproject/abi";
+import type { LogDescription } from '@ethersproject/abi';
 import { expect, use } from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import sinonChai from 'sinon-chai';
-import { RelayTransactionEvents, relayTransactionHandler } from '../../src/handlers/RelayProvider'
-
+import {
+  RelayTransactionEvents,
+  relayTransactionHandler,
+} from '../../src/handlers/RelayProvider';
 
 use(sinonChai);
 use(chaiAsPromised);
 
 describe('Handler:RelayProvider', function () {
+  describe('relayTransactionhandler', function () {
+    it('should handle TransactionRelayedButRevertedByRecipient', function () {
+      const revertReason = 'Transaction reverted because of some reason';
+      const dummyLogDescription = {
+        name: 'TransactionRelayedButRevertedByRecipient',
+        args: {
+          reason: revertReason,
+        },
+      } as unknown as LogDescription;
 
-    describe('relayTransactionhandler', function () {
+      const relayStatus = relayTransactionHandler.process(
+        dummyLogDescription.name as RelayTransactionEvents,
+        dummyLogDescription
+      );
 
-        it('should handle TransactionRelayedButRevertedByRecipient', function () {
+      expect(relayStatus.transactionRelayed).to.be.false;
+      expect(relayStatus.relayRevertedOnRecipient).to.be.true;
+      expect(relayStatus.reason).to.eq(revertReason);
+    });
 
-            const revertReason = 'Transaction reverted because of some reason';
-            const dummyLogDescription = {
-                name: 'TransactionRelayedButRevertedByRecipient',
-                args: {
-                  reason: revertReason
-                }
-            } as unknown as LogDescription
-    
-            const relayStatus = relayTransactionHandler.process(dummyLogDescription.name as RelayTransactionEvents, dummyLogDescription);
-            
-            expect(relayStatus.transactionRelayed).to.be.false;
-            expect(relayStatus.relayRevertedOnRecipient).to.be.true;
-            expect(relayStatus.reason).to.eq(revertReason);
-        })
+    it('should handle TransactionRelayed', function () {
+      const dummyLogDescription = {
+        name: 'TransactionRelayed',
+      } as unknown as LogDescription;
 
-        it('should handle TransactionRelayed', function () {
+      const relayStatus = relayTransactionHandler.process(
+        dummyLogDescription.name as RelayTransactionEvents,
+        dummyLogDescription
+      );
 
-            const dummyLogDescription = {
-                name: 'TransactionRelayed',
-              } as unknown as LogDescription
-    
-            const relayStatus = relayTransactionHandler.process(dummyLogDescription.name as RelayTransactionEvents, dummyLogDescription);
-            
-            expect(relayStatus.transactionRelayed).to.be.true;
-            expect(relayStatus.relayRevertedOnRecipient).to.be.false;
-            expect(relayStatus.reason).to.be.undefined;
-        })
+      expect(relayStatus.transactionRelayed).to.be.true;
+      expect(relayStatus.relayRevertedOnRecipient).to.be.false;
+      expect(relayStatus.reason).to.be.undefined;
+    });
 
-        it('should handle Deployed event', function () {
+    it('should handle Deployed event', function () {
+      const dummyLogDescription = {
+        name: 'Deployed',
+      } as unknown as LogDescription;
 
-            const dummyLogDescription = {
-                name: 'Deployed',
-            } as unknown as LogDescription
-    
-            const relayStatus = relayTransactionHandler.process(dummyLogDescription.name as RelayTransactionEvents, dummyLogDescription);
-            
-            expect(relayStatus.transactionRelayed).to.be.true;
-            expect(relayStatus.relayRevertedOnRecipient).to.be.false;
-        })
+      const relayStatus = relayTransactionHandler.process(
+        dummyLogDescription.name as RelayTransactionEvents,
+        dummyLogDescription
+      );
 
-        it('should provide a default behaviour for unsupported events', function () {
+      expect(relayStatus.transactionRelayed).to.be.true;
+      expect(relayStatus.relayRevertedOnRecipient).to.be.false;
+    });
 
-            const relayStatus = relayTransactionHandler.default();
-            
-            expect(relayStatus.transactionRelayed).to.be.false;
-            expect(relayStatus.relayRevertedOnRecipient).to.be.false;
-            expect(relayStatus.reason).to.eq('Neither TransactionRelayed, Deployed, nor TransactionRelayedButRevertedByRecipient events found. This might be a non-enveloping transaction.');
-        })
-    })
+    it('should provide a default behaviour for unsupported events', function () {
+      const relayStatus = relayTransactionHandler.default();
 
+      expect(relayStatus.transactionRelayed).to.be.false;
+      expect(relayStatus.relayRevertedOnRecipient).to.be.false;
+      expect(relayStatus.reason).to.eq(
+        'Neither TransactionRelayed, Deployed, nor TransactionRelayedButRevertedByRecipient events found. This might be a non-enveloping transaction.'
+      );
+    });
+  });
 });
